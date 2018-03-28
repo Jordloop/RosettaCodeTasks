d2c496549130045b059e20d94e97b912d2f46217
JordLoop <Jordloop@gmail.com>
2018-03-28 16:47:46 -0700

    add and complete Array.cs task

--- Basics/Arrays.cs
+++ Basics/Arrays.cs
@@ -14,11 +14,22 @@
 // http://www.rosettacode.org/wiki/Arrays
 
 using System;
+using System.Collections.Generic;
 
 class Project
 {
     static void Main()
     {
+        //Create a list with assigned values
+        List<string> fruits = new List<string>() {"Raspberry", "Blueberry", "Pineapple" };
 
+        //Assign new value to list
+        fruits.Add("Apple");
+
+        //Access the 2nd element of the list
+        string blueberry = fruits[1];
+
+        //Print each list element to console
+        foreach(string fruit in fruits) Console.WriteLine(fruit);
     }
 }
