8c4fce1fef3a2e5ce52e5c690715d6f077d34098
JordLoop <Jordloop@gmail.com>
2018-03-28 16:25:37 -0700

    add Arrays.cs

---
+++ Basics/Arrays.cs
@@ -0,0 +1,24 @@
+// Task
+// Show basic array syntax in your language.
+
+// Basically, create an array, assign a value to it, and retrieve an element   (if available, show both fixed-length arrays and dynamic arrays, pushing a value into it).
+
+// Please discuss at Village Pump:   Arrays.
+
+// Please merge code in from these obsolete tasks:
+
+//   Creating an Array
+//   Assigning Values to an Array
+//   Retrieving an Element of an Array
+
+// http://www.rosettacode.org/wiki/Arrays
+
+using System;
+
+class Project
+{
+    static void Main()
+    {
+
+    }
+}
