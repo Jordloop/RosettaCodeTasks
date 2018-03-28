e0a9a13cb415ba5c1f559a1b4bc0c0fd95f67ab0
JordLoop <Jordloop@gmail.com>
2018-03-28 16:52:12 -0700

    add Boolean_Values.cs task

---
+++ Basics/Boolean_Values.cs
@@ -0,0 +1,14 @@
+// Task
+// Show how to represent the boolean states "true" and "false" in a language.
+
+// If other objects represent "true" or "false" in conditionals, note it.
+
+// http://www.rosettacode.org/wiki/Boolean_values
+
+class Program 
+{
+    static void Main()
+    {
+        
+    }
+}
