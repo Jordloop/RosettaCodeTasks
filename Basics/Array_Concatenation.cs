f1429b3383620e87ad06b95c14ff6f7d48a8cb0b
JordLoop <Jordloop@gmail.com>
2018-03-28 16:10:06 -0700

    completed Array_Concatenation.cs task

--- Basics/Array_Concatenation.cs
+++ Basics/Array_Concatenation.cs
@@ -10,8 +10,22 @@ using System;
 
 class Program 
 {
-        static void Main()
+    static void Main()
+    {
+        // Instantiate arrays
+        int[] arr1 = new int[] { 1, 2, 3};
+        int[] arr2 = new int[] { 4, 5, 6, 7};
+
+        // Instantiates a new array with the proper size
+        int[] arr3 = new int[arr1.Length + arr2.Length];
+
+        // Copies contents of arr1 to arr3 starting at inex 0
+        arr1.CopyTo(arr3, 0);
+        arr2.CopyTo(arr3, arr1.Length);
+
+        foreach(int n in arr3)
         {
-            
+            Console.WriteLine(n.ToString());
         }
+    }
 }
