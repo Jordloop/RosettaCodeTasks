e0a55d9ed95c290577f5c62d771bd3ca96d85295
JordLoop <Jordloop@gmail.com>
2018-03-28 16:18:13 -0700

    add and complete Array_Length.cs

---
+++ Basics/Array_Length.cs
@@ -0,0 +1,18 @@
+// Task
+// Determine the amount of elements in an array.
+
+// As an example use an array holding the strings 'apple' and 'orange'.
+
+// http://www.rosettacode.org/wiki/Array_length
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        string[] arr = new string[] {"apple", "orange"};
+
+        Console.WriteLine(arr.Length);
+    }
+}
