9cbbdb7bc3369a4309b6135e799a4d2e94ce1673
JordLoop <Jordloop@gmail.com>
2018-03-29 12:50:17 -0700

    delete Boolean_Values.exe, add Check_That_File_Exists.cs

---
+++ Basics/Check_That_File_Exists.cs
@@ -0,0 +1,18 @@
+// Task
+// Verify that a file called     input.txt     and   a directory called     docs     exist.
+
+
+// This should be done twice:  
+
+//   once for the current working directory,   and
+//   once for a file and a directory in the filesystem root.
+
+//   http://www.rosettacode.org/wiki/Check_that_file_exists
+
+class Project
+{
+    static void Main()
+    {
+        
+    }
+}
