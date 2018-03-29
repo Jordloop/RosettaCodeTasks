3cc52925e2f10fb6b553f856f6727aa6609ad0a1
JordLoop <Jordloop@gmail.com>
2018-03-29 13:12:51 -0700

    add Conditional_Structures.cs

---
+++ Basics/Conditional_Structures.cs
@@ -0,0 +1,16 @@
+// Task
+// List the   conditional structures   offered by a programming language.
+
+// Common conditional structures are     if-then-else     and     switch. 
+
+// http://www.rosettacode.org/wiki/Conditional_structures
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+
+    }
+}
