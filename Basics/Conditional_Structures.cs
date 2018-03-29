4c0934225b5b3c988404537c620ff414916ceea2
JordLoop <Jordloop@gmail.com>
2018-03-29 13:35:43 -0700

    complete Conditional_Structures.cs tasks

--- Basics/Conditional_Structures.cs
+++ Basics/Conditional_Structures.cs
@@ -11,6 +11,39 @@ class Program
 {
     static void Main()
     {
+        bool condition = false;
+        int caseSwitch = 42;
+        
+// if-else statement
+        if(condition)
+        {
+            Console.WriteLine("Condition evaluated true.");
+        }
+        else
+        {
+            Console.WriteLine("Condition evaluated false.");
+        }
+
+// ternary opperator
+        string Ternary_Operator = condition ? "Condition evaluated true." : "Condition evaluated false.";
+        Console.WriteLine(Ternary_Operator);
+
+// switch statement
+        switch(caseSwitch)
+        {
+            case 1:
+                Console.WriteLine("case 1");
+                break;
+            case 2:
+                Console.WriteLine("case 2");
+                break;
+            case 3:
+                Console.WriteLine("case 3");
+                break;
+            default:
+                Console.WriteLine("Default case");
+                break;
+        }
 
     }
 }
