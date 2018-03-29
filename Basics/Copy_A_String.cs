daccacde8de350aec2892e15fbba0f27ddc127bc
JordLoop <Jordloop@gmail.com>
2018-03-29 13:47:58 -0700

    add and complete Copy_A_String.cs task

---
+++ Basics/Copy_A_String.cs
@@ -0,0 +1,16 @@
+// Task
+// Where it is relevant, distinguish between copying the contents of a string versus making
+// an additional reference to an existing string. 
+
+// http://www.rosettacode.org/wiki/Copy_a_string
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        string Str = "I am a string";
+        string Copy_Of_Str = Str;
+    }
+}
